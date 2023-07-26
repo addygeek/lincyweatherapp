1239b89a5294c3641f16298ef8c7c41a12385dc2
Aditya Kumar <118501211+addygeek@users.noreply.github.com>
2023-07-26 11:43:24 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# Weather-App
+Check the weather around you and all over the world at a glance.
+Rely on the accurate weather forecast and adjust your schedule to the weather coming in. You won’t even have to look out the window as the app will make you feel like you are already outside!
+
+Weather is sometimes difficult to predict. This accurate weather app allows to find out a detailed forecast wherever you are, for any time of the day by tapping on the icons:
+- Day information
+- Current and Humidity Information
+- Wind speed and direction
+- Pressure and precipitation information 
+### Sample-Image
+![Screenshot 2023-07-25 094602](https://github.com/LincyBainiwal/WeatherApp2.0/assets/129520182/cb29afeb-4552-4dd1-a1dc-2f0c0f4c4fa9)
+
+![Screenshot 2023-07-25 094618](https://github.com/LincyBainiwal/WeatherApp2.0/assets/129520182/3e372d12-76e5-432d-bae2-d271469ea2e3)
---
+++ script.js
@@ -0,0 +1,82 @@
+let searchInp = document.querySelector('.weather_search');
+let city = document.querySelector('.weather_city');
+let day = document.querySelector('.weather_day');
+let humidity = document.querySelector('.weather_indicator--humidity>.value');
+let wind = document.querySelector('.weather_indicator--wind>.value');
+let pressure = document.querySelector('.weather_indicator--pressure>.value');
+let image = document.querySelector('.weather_image');
+let temperature = document.querySelector('.weather_temperature>.value');
+let forecastBlock = document.querySelector('.weather_forecast');
+const API_KEY = 'dcd60eb8cec748c18ba65041232207';
+
+let getWeatherByCityName = async (city) =>{
+    let url = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${city}&days=7&aqi=no&alerts=no`
+    let response = await fetch(url);
+    let weatherData = await response.json();
+    return weatherData;
+};
+let updateCurrentWeather = (data) => {
+    city.textContent = data.location.name + ', ' + data.location.country;
+    day.textContent = dayOfWeek();
+   humidity.textContent = data.current.humidity + '%';
+   pressure.textContent = data.current.pressure_mb + 'mph';
+    wind.textContent = data.current.wind_dir + ', ' + data.current.wind_mph;
+    temperature.textContent = data.current.temp_c ;
+    image.src = data.current.condition.icon;
+    image.alt = data.current.condition.text;
+};
+let updateForecast = (data) => {
+    forecastBlock.innerHTML = '';
+    for (let i = 1; i <= 6; i++) {
+        let forecastDate = new Date(data.forecast.forecastday[i].date);
+        let forecastDay = forecastDate.toLocaleDateString('en-EN', { weekday: 'long' });
+        let  forecastTemp = data.forecast.forecastday[i].day.avgtemp_c;
+        let forecastCondition = data.forecast.forecastday[i].day.condition.icon;
+
+        let forecastItem = document.createElement('article');
+        forecastItem.classList.add('weather_forecast_item');
+        forecastItem.innerHTML = `
+        <img src="${forecastCondition}" alt="${forecastDay}" class="weather_forecast_icon">
+        <h3 class="weather_forecast_day">${forecastDay}</h3>
+        <p class="weather_forecast_temperature"><span class="value">${forecastTemp}</span>&deg; C</p>
+        `;
+
+        forecastBlock.appendChild(forecastItem);
+    }
+ }; 
+let dayOfWeek = () =>{
+     return new Date().toLocaleDateString('en-EN',{'weekday':'long'});
+};
+searchInp.addEventListener('keydown' , async (e) => {
+    if(e.keyCode === 13){
+       let weatherData =  await  getWeatherByCityName(searchInp.value);
+       updateCurrentWeather(weatherData);
+       updateForecast(weatherData);
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
